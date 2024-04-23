2d9e09b22ec7129f5bef6bd90d1ef02cc905d743
manastelavane777 <manastelavane@ternaengg.ac.in>
2024-04-23 14:11:45 +0530

    feat: upgraded to version 4 of weaviate

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 import weaviate
+import weaviate.classes as wvc
+from weaviate.classes.query import MetadataQuery
 
 app = Flask(__name__) 
 
@@ -7,107 +9,74 @@ app = Flask(__name__)
 weaviate_url = "https://turtle-vinci-mtccq694.weaviate.network"
 api_key = "ZElsNdQp6rOcmXzhZe7WuknuVqN6tMyKvSou"
 
-client = weaviate.Client(
-    url=weaviate_url,
-    auth_client_secret=weaviate.AuthApiKey(api_key=api_key),
-    additional_headers={
+client = weaviate.connect_to_wcs(
+    cluster_url=weaviate_url,
+    auth_credentials=weaviate.auth.AuthApiKey(api_key=api_key),
+    headers={
         "X-HuggingFace-Api-Key": "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"  # Replace with your inference API key
     }
 )
 
 
 @app.route('/doodle/')
-def query_handler():
+def query_handler_doodle():
     # Retrieve query parameters from the request
     query_word = request.args.get('query')
     limit = int(request.args.get('limit', 1))  # Default limit is 1 if not provided
     # Perform a text-based similarity search with the query word
     print(f"Querying Weaviate for '{query_word}' with limit {limit}")
-    query_result = (
-        client.query
-        .get("ImageStrokeDoodle", ["nameOfImage","strokeOfImage"])
-        .with_near_text({
-            "concepts": [query_word],
-        })
-        .with_limit(limit)
-        .do()
-    )
-
+    imageStrokeDoodle=client.collections.get("ImageStrokeDoodle")
+    response=imageStrokeDoodle.query.near_text(query=query_word,limit=limit)
     # Extract relevant information from the query result
     response_data = []
-    for result_object in query_result.get('data', {}).get('Get', {}).get('ImageStrokeDoodle', []):
+    for result_object in response.objects:
         # Append the entire imageStroke array to the response
-        response_data.append(result_object)
+        response_data.append(result_object.properties)
     return jsonify(response_data)
+    
 
 @app.route('/doodle/add_stroke', methods=['POST'])
-def add_stroke():
+def add_stroke_doodle():
     # Retrieve word and stroke data from the request
     word = request.json.get('word')
     strokes = request.json.get('strokes')
     print(f"Adding stroke for '{word}'")
-    where_filter = {
-        "path": ["nameOfImage"],
-        "operator": "Equal",
-        "valueText": word,
-    }
-    query_result = (
-        client.query
-        .get("ImageStrokeDoodle", ["nameOfImage","strokeOfImage"])
-        .with_near_text({
-            "concepts": [word],
-        })
-        .with_where(where_filter)
-        .with_additional(["id"])
-        .do()
-    )
-    # print(query_result)
+    imageStrokeDoodle=client.collections.get("ImageStrokeDoodle")
+    response=imageStrokeDoodle.query.near_text(query=word,filters=wvc.query.Filter.by_property("nameOfImage").equal(word))
+    data=response.objects
+    # ----------------ADD ADDITIONAL PARAMETER ID as UIDD -------------
     
-    # Check if there are any results
-    data = query_result.get('data', {}).get('Get', {}).get('ImageStrokeDoodle', [])
-    # print(len(data[0].get('strokeOfImage', [])))
-    # print(data)
+    print(data)
     if len(data) > 0:
         print("Updating existing stroke")
         # Update the existing stroke data
-        existing_strokes = data[0].get('strokeOfImage', [])
+        existing_strokes = data[0].properties.get('strokeOfImage', [])
         existing_strokes.append(strokes)
-        uuid=data[0].get('_additional', {}).get('id', '')
-        # print(uuid)
-        client.data_object.update(
-            class_name="ImageStrokeDoodle",
-            uuid=uuid,
-            data_object={"strokeOfImage": existing_strokes}
-        )
+        print(existing_strokes)
+        uuid=data[0].uuid
+        print(uuid)
+        imageStrokeDoodle.data.update(uuid=uuid, properties={"strokeOfImage": existing_strokes})
     else:
         print("Adding new stroke")
         # Add a new stroke data
-        client.data_object.create(class_name="ImageStrokeDoodle", data_object={"nameOfImage": word, "strokeOfImage": [strokes]})
+        imageStrokeDoodle=client.collections.get("ImageStrokeDoodle")
+        uuid=imageStrokeDoodle.data.insert({"nameOfImage": word, "strokeOfImage": [strokes]})
     return jsonify({'message': 'Stroke added successfully'})
 
 @app.route('/weaviate/')
-def query_handler():
+def query_handler_weaviate():
     # Retrieve query parameters from the request
     query_word = request.args.get('query')
     limit = int(request.args.get('limit', 1))  # Default limit is 1 if not provided
     # Perform a text-based similarity search with the query word
     print(f"Querying Weaviate for '{query_word}' with limit {limit}")
-    query_result = (
-        client.query
-        .get("ImageStroke", ["nameOfImage"])
-        .with_near_text({
-            "concepts": [query_word],
-            "distance": 0.7
-        })
-        .with_limit(limit)
-        .do()
-    )
-    print(query_result)
+    imageStroke=client.collections.get("ImageStroke")
+    response=imageStroke.query.near_text(query=query_word,distance=0.7,limit=limit,return_metadata=MetadataQuery(distance=True))
     # Extract relevant information from the query result
     response_data = []
-    for result_object in query_result.get('data', {}).get('Get', {}).get('ImageStroke', []):
+    for result_object in response.objects:
         # Append the entire imageStroke array to the response
-        response_data.append(result_object)
+        response_data.append(result_object.properties)
     return jsonify(response_data)
 
 if __name__ == '__main__':
